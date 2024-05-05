af1564d417162cfe561b385efe22ee77be4b3ce9
EsraaRedaElgiz <110185098+EsraaRedaElgiz@users.noreply.github.com>
2024-05-06 02:34:51 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -9,6 +9,10 @@ window.addEventListener("load", () => {
     let starsSlider = document.querySelector("#starsSlider")
     let circleChart = document.querySelector("#circleChart")
     let chartSlider = document.querySelector("#chartSlider")
+    let rightButtonFifthSection = document.querySelector("#rightButton")
+    let leftButtonFifthSection = document.querySelector("#leftButton")
+    let sliderImgs = document.querySelectorAll('.imgsSlider img');
+
     dropDownButton.addEventListener("click", () => {
         document.querySelector(".dropdown-content").classList.toggle("show")
     });
@@ -38,21 +42,21 @@ window.addEventListener("load", () => {
 
     promoterScoreSlider.addEventListener("click", () => {
         promoterScoreSlider.classList.add("hide")
-        let parts=["green","yellow","red"]
-        let emojes=["sad","neutral","love"]
-        for(let i=0;i<parts.length;i++){
+        let parts = ["green", "yellow", "red"]
+        let emojes = ["sad", "neutral", "love"]
+        for (let i = 0; i < parts.length; i++) {
             document.querySelector(`#${parts[i]}Part`).classList.add(`${parts[i]}`)
 
         }
-        for(let i=0;i<emojes.length;i++){
+        for (let i = 0; i < emojes.length; i++) {
             document.querySelector(`#${emojes[i]}`).classList.add("emoje")
 
         }
-       
+
     })
     starsSlider.addEventListener("click", () => {
-        let arrayOfElementes=["first","second","third","forth","five"]
-        for(let i=0;i<arrayOfElementes.length;i++){
+        let arrayOfElementes = ["first", "second", "third", "forth", "five"]
+        for (let i = 0; i < arrayOfElementes.length; i++) {
             document.querySelector(`#${arrayOfElementes[i]}StarPurpleSlider`).classList.add(`${arrayOfElementes[i]}StarPurple`)
             document.querySelector(`#${arrayOfElementes[i]}StarGraySlider`).classList.add(`${arrayOfElementes[i]}StarGrey`)
         }
@@ -60,8 +64,8 @@ window.addEventListener("load", () => {
     })
     circleChart.addEventListener("click", () => {
         circleChart.classList.add("circleChart")
-        let arrayOfImgAndClassNumber=["first","second","third","forth","last"]
-        for(let i=0;i<arrayOfImgAndClassNumber.length;i++){
+        let arrayOfImgAndClassNumber = ["first", "second", "third", "forth", "last"]
+        for (let i = 0; i < arrayOfImgAndClassNumber.length; i++) {
             document.querySelector(`#${arrayOfImgAndClassNumber[i]}Img`).classList.add(`${arrayOfImgAndClassNumber[i]}Img`)
         }
 
@@ -71,15 +75,35 @@ window.addEventListener("load", () => {
         for (let i = 0; i < allEmojes.length; i++) {
             allEmojes[i].setAttribute("style", "opacity:1")
         }
-        for(let i=0;i<8;i++){
-            if(i==4||i==5||i==6){
-                document.querySelector(`#answer${i+1}`).classList.add("answer1")
+        for (let i = 0; i < 8; i++) {
+            if (i == 4 || i == 5 || i == 6) {
+                document.querySelector(`#answer${i + 1}`).classList.add("answer1")
 
-            }else{
-                document.querySelector(`#answer${i+1}`).classList.add(`answer${i+1}`)
+            } else {
+                document.querySelector(`#answer${i + 1}`).classList.add(`answer${i + 1}`)
 
             }
         }
+    })
+    const sliderFunction=()=>{
+        if (leftButtonFifthSection.disabled == false
+        ) {
+            sliderImgs[1].style.opacity = 1
+            sliderImgs[2].style.opacity = 0;
+            leftButtonFifthSection.style.backgroundColor = "#dddddd"
+            leftButtonFifthSection.disabled = true
+        } else {
+            sliderImgs[1].style.opacity = 0
+            sliderImgs[2].style.opacity = 1;
+            leftButtonFifthSection.style.backgroundColor = "#fff"
+            leftButtonFifthSection.disabled = false
+        }
+    }
+    rightButtonFifthSection.addEventListener("click", () => {
+       sliderFunction()
+    })
+    leftButtonFifthSection.addEventListener("click", () => {
+        sliderFunction()
     })
 
 })
